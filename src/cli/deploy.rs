498b926271759107d61725753494596a97e94241
Liam <liam@evervault.com>
2022-08-03 14:55:42 +0100

    extend init command, add signing info header to toml

--- src/cli/deploy.rs
+++ src/cli/deploy.rs
@@ -1,16 +1,20 @@
 use clap::Parser;
 
-/// Manage Cage signing certificates
+/// Deploy a Cage from a toml file.
 #[derive(Debug, Parser)]
 #[clap(name = "deploy", about)]
 pub struct DeployArgs {
-    /// Path to eif to deploy
-    #[clap(long = "eif")]
-    pub eif_path: String,
+    /// Path to cage.toml config file
+    #[clap(short = 'c', long = "config", default_value = "./cage.toml")]
+    pub config: String,
 
-    /// Name of the Cage to deploy
-    #[clap(long = "name")]
-    pub name: String,
+    /// Path to Dockerfile for Cage. Will override any dockerfile specified in the .toml file.
+    #[clap(short = 'f', long = "file", default_value = "./Dockerfile")]
+    pub dockerfile: String,
+
+    /// Path to use for docker context
+    #[clap(default_value = ".")]
+    pub context_path: String,
 }
 
 pub async fn run(_deploy_args: DeployArgs) {}
--- src/config.rs
+++ src/config.rs
@@ -8,9 +8,19 @@ pub struct EgressSettings {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct SigningInfo {
+    #[serde(rename = "certPath")]
+    pub cert: Option<String>,
+    #[serde(rename = "keyPath")]
+    pub key: Option<String>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct CageConfig {
     pub name: String,
     pub debug: bool,
+    pub dockerfile: Option<String>,
     pub egress: EgressSettings,
+    pub signing: Option<SigningInfo>,
     pub attestation: Option<EIFMeasurements>,
 }
