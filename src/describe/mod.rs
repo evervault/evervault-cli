24affdcc5907c44bf171851c6c6381f4824bb4b5
Hannah Neary <hannah@evervault.com>
2022-08-16 21:09:18 +0100

    Add eif-path flag to deploy

--- src/describe/mod.rs
+++ src/describe/mod.rs
@@ -1,5 +1,4 @@
 mod error;
-use crate::common::resolve_output_path;
 
 use crate::docker::{error::DockerError, utils::verify_docker_is_running};
 use crate::enclave;
@@ -14,10 +13,6 @@ pub fn describe_eif(eif_path: &str) -> Result<enclave::DescribeEif, DescribeErro
     if !verify_docker_is_running()? {
         return Err(DockerError::DaemonNotRunning.into());
     }
-
-    let supplied_path: Option<&str> = None;
-    let output_path = resolve_output_path(supplied_path).unwrap();
-    enclave::build_nitro_cli_image(output_path.path(), None, false).unwrap();
 
     let description = enclave::describe_eif(&absolute_path).unwrap();
 
