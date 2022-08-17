c0b546091f3db34a324c8da5a628503d322c8e52
Hannah Neary <hannah@evervault.com>
2022-08-17 09:51:06 +0100

    Refactor errors

--- src/describe/error.rs
+++ src/describe/error.rs
@@ -6,5 +6,5 @@ pub enum DescribeError {
     #[error("Failed to describe enclave image file — {0}")]
     DockerError(#[from] DockerError),
     #[error("Could not find eif at {0}")]
-    EIFNotFound(std::path::PathBuf),
+    EIFNotFound(std::path::PathBuf)
 }
