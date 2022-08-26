ee24c718727adbad53fdd34ce37c0b0498405507
Donal Tuohy <donaltuohy@gmail.com>
2022-08-26 09:47:51 +0100

    [SCL-430] fmt

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,11 +1,11 @@
 use clap::Subcommand;
 pub mod build;
 pub mod cert;
+pub mod delete;
 pub mod deploy;
 pub mod describe;
 pub mod init;
 pub mod list;
-pub mod delete;
 
 #[derive(Debug, Subcommand)]
 pub enum Command {
