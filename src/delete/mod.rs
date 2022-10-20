3eb660911671dbdd331e70af230e60458f45e851
Liam <liam@evervault.com>
2022-10-20 14:46:02 +0100

    format project

--- src/delete/mod.rs
+++ src/delete/mod.rs
@@ -2,7 +2,7 @@ use crate::api;
 use crate::api::cage::CagesClient;
 use crate::api::AuthMode;
 use crate::config::CageConfig;
-use crate::progress::{get_tracker, ProgressLogger, StatusReport, poll_fn_and_report_status};
+use crate::progress::{get_tracker, poll_fn_and_report_status, ProgressLogger, StatusReport};
 mod error;
 use error::DeleteError;
 
@@ -31,20 +31,29 @@ pub async fn delete_cage(config: &str, api_key: &str) -> Result<(), DeleteError>
 }
 
 async fn watch_deletion(cage_api: CagesClient, cage_uuid: &str, progress_bar: impl ProgressLogger) {
-  async fn check_delete_status(cage_api: CagesClient, args: Vec<String>) -> Result<StatusReport, DeleteError> {
-    let cage_uuid = args.get(0).unwrap();
-    match cage_api.get_cage(cage_uuid).await {
-      Ok(cage_response) if cage_response.is_deleted() => {
-        Ok(StatusReport::Complete("Cage deleted!".to_string()))
-      },
-      Ok(_) => Ok(StatusReport::NoOp),
-      Err(e) => {
-        log::error!("Unable to retrieve deletion status. Error: {:?}", e);
-        Ok(StatusReport::Failed)
-      }
+    async fn check_delete_status(
+        cage_api: CagesClient,
+        args: Vec<String>,
+    ) -> Result<StatusReport, DeleteError> {
+        let cage_uuid = args.get(0).unwrap();
+        match cage_api.get_cage(cage_uuid).await {
+            Ok(cage_response) if cage_response.is_deleted() => {
+                Ok(StatusReport::Complete("Cage deleted!".to_string()))
+            }
+            Ok(_) => Ok(StatusReport::NoOp),
+            Err(e) => {
+                log::error!("Unable to retrieve deletion status. Error: {:?}", e);
+                Ok(StatusReport::Failed)
+            }
+        }
     }
-  }
-  
-  let check_delete_args = vec![cage_uuid.to_string()];
-  let _ = poll_fn_and_report_status(cage_api, check_delete_args, check_delete_status, progress_bar).await;
+
+    let check_delete_args = vec![cage_uuid.to_string()];
+    let _ = poll_fn_and_report_status(
+        cage_api,
+        check_delete_args,
+        check_delete_status,
+        progress_bar,
+    )
+    .await;
 }
