aa2a7196d026a685c8f74b2f3b0f296f8f09aaf2
Hannah Neary <hannah@evervault.com>
2022-12-05 10:39:22 +0000

    Use toml on encrypt

--- src/cli/encrypt.rs
+++ src/cli/encrypt.rs
@@ -1,4 +1,7 @@
-use crate::encrypt;
+use crate::{
+    config::CageConfig,
+    encrypt::{self, EncryptError}
+};
 use clap::Parser;
 
 #[derive(Clone, Debug, clap::ArgEnum)]
@@ -8,11 +11,10 @@ pub enum CurveName {
 }
 
 /// Encrypt a string
-#[derive(Debug, Parser)]
+#[derive(Debug, Parser, Clone)]
 #[clap(name = "encrypt", about)]
 pub struct EncryptArgs {
     // Plaintext value to encrypt
-    #[clap(long = "value")]
     pub value: String,
 
     // Curve to use, options are nist or koblitz
@@ -20,28 +22,54 @@ pub struct EncryptArgs {
     pub curve: CurveName,
 
     #[clap(long = "team_uuid")]
-    pub team_uuid: String,
+    pub team_uuid: Option<String>,
 
     #[clap(long = "app_uuid")]
-    pub app_uuid: String,
+    pub app_uuid: Option<String>,
+
+    // Path to cage.toml config file
+    #[clap(short = 'c', long = "config", default_value = "./cage.toml")]
+    pub config: String,
 }
 
 pub async fn run(encrypt_args: EncryptArgs) -> exitcode::ExitCode {
-    match encrypt::encrypt(
-        encrypt_args.value,
-        encrypt_args.team_uuid,
-        encrypt_args.app_uuid,
-        encrypt_args.curve,
-    )
-    .await
-    {
+    let (team_uuid, app_uuid) = match get_cage_details(encrypt_args.clone()) {
+        Ok((team_uuid, app_uuid))=> (team_uuid, app_uuid),
+        Err(e) => {
+            log::error!("Config error {}", e);
+            return exitcode::SOFTWARE
+        }
+    };
+    match encrypt::encrypt(encrypt_args.value, team_uuid, app_uuid, encrypt_args.curve).await {
         Ok(encrypted_string) => {
-            log::info!("{}", encrypted_string);
+            println!("{}", encrypted_string);
             exitcode::OK
         }
         Err(e) => {
             log::info!("{}", e);
             exitcode::SOFTWARE
         }
+    }
+}
+
+fn get_cage_details(encrypt_args: EncryptArgs) -> Result<(String, String), EncryptError> {
+    if encrypt_args.team_uuid.is_none() || encrypt_args.app_uuid.is_none() {
+        let cage_config = CageConfig::try_from_filepath(&encrypt_args.config)?;
+
+        if cage_config.app_uuid.is_none()
+            || cage_config.team_uuid.is_none()
+        {
+            return Err(EncryptError::MissingUuid);
+        } else {
+            Ok((
+                cage_config.team_uuid.unwrap(),
+                cage_config.app_uuid.unwrap(),
+            ))
+        }
+    } else {
+        Ok((
+            encrypt_args.team_uuid.unwrap(),
+            encrypt_args.app_uuid.unwrap(),
+        ))
     }
 }
