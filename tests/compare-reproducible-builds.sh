f615d7b8bd1912779a07332db20ca070c24021ea
Liam <liam@evervault.com>
2023-01-06 12:20:09 +0000

    Update kaniko functions to respect non tty stdout, update tests to read pre-written cage.toml, add test for comparing subsequent reproducible builds

---
+++ tests/compare-reproducible-builds.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cargo run -- build --reproducible -c test.cage.toml > first-build.json
+# shasum gives us <SHA> <FILENAME> so need to extract the first word
+FIRST_SHA=`shasum first-build.json | cut -d" " -f1`
+
+cargo run -- build --reproducible -c test.cage.toml > second-build.json
+SECOND_SHA=`shasum second-build.json | cut -d" " -f1`
+
+if [ "$FIRST_SHA" = "$SECOND_SHA" ]; then
+  echo "PCRs match!"
+  exit 0
+else
+  echo "PCRs aren't equal!"
+  echo "$FIRST_SHA $SECOND_SHA"
+  diff first-build.json second-build.json
+  exit 1
+fi
