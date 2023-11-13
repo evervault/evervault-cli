7a07d6b821c4097dd720f4f5eb79a5423cded99d
Liam <liam@evervault.com>
2023-11-13 18:18:05 +0000

    fix: update installer to include major paths, correct paths for s3 uploads, remove leading v on major version

--- scripts/generate-installer.sh
+++ scripts/generate-installer.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 pattern="s/{{version}}/$1/"
+major_pattern="s/{{major}}/$2/"
 
 sed -e "$pattern" ./scripts/install.template > ./scripts/install
+sed -e "$major_pattern" ./scripts/install > ./scripts/install
 
 sed -e "$pattern" ./scripts/version.template > ./scripts/version
