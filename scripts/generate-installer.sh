554012e8cf65e4772e4be6086f299c6588641399
Hannah Neary <92307259+hanneary@users.noreply.github.com>
2023-12-06 10:18:42 +0000

    Publish staging CLI on push (#65)

--- scripts/generate-installer.sh
+++ scripts/generate-installer.sh
@@ -2,8 +2,10 @@
 
 pattern="s/{{version}}/$1/"
 major_pattern="s/{{major}}/$2/"
+domain_pattern="s/{{domain}}/$3/"
 
-sed -e "$pattern" ./scripts/install.template > ./scripts/install
-sed -e "$major_pattern" ./scripts/install > ./scripts/install
+sed -e "$pattern" ./scripts/install.template > ./scripts/install-temp
+sed -e "$domain_pattern" ./scripts/install-temp > ./scripts/install-temp-domain
+sed -e "$major_pattern" ./scripts/install-temp-domain > ./scripts/install
 
 sed -e "$pattern" ./scripts/version.template > ./scripts/version
