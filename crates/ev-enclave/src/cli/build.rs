7ae6dcf77ce89e80eb5a4de99f064b59de29c892
Hannah Neary <hannah@evervault.com>
2024-02-07 15:29:21 +0000

    parse version from enclave.Dockerfile for repro builds

--- crates/ev-enclave/src/cli/build.rs
+++ crates/ev-enclave/src/cli/build.rs
@@ -1,9 +1,9 @@
-use crate::api::assets::AssetsClient;
 use crate::build::build_enclave_image_file;
 use crate::common::{prepare_build_args, CliError};
 use crate::config::{read_and_validate_config, BuildTimeConfig};
 use crate::docker::command::get_source_date_epoch;
 use crate::version::check_version;
+use crate::version::get_runtime_and_installer_version;
 use clap::Parser;
 
 /// Build an Enclave from a Dockerfile
@@ -97,22 +97,16 @@ pub async fn run(build_args: BuildArgs) -> exitcode::ExitCode {
         .as_ref()
         .map(|args| args.iter().map(AsRef::as_ref).collect());
 
-    let enclave_build_assets_client = AssetsClient::new();
-    let data_plane_version = match enclave_build_assets_client.get_data_plane_version().await {
-        Ok(version) => version,
-        Err(e) => {
-            log::error!("Failed to retrieve the latest data plane version - {e:?}");
-            return e.exitcode();
-        }
-    };
-
-    let installer_version = match enclave_build_assets_client.get_installer_version().await {
-        Ok(version) => version,
-        Err(e) => {
-            log::error!("Failed to retrieve the latest installer version - {e:?}");
-            return e.exitcode();
-        }
-    };
+    let (data_plane_version, installer_version) =
+        match get_runtime_and_installer_version(build_args.from_existing.clone()).await {
+            Ok(versions) => versions,
+            Err(e) => {
+                log::error!(
+                    "Failed to retrieve the latest data plane and installer versions - {e:?}"
+                );
+                return e.exitcode();
+            }
+        };
 
     let timestamp = get_source_date_epoch();
 
