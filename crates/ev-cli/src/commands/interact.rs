c14584b08aa87dcc3ec507542c75f952943c95fa
Mark Doyle <39464373+doylemark@users.noreply.github.com>
2024-05-20 14:16:23 +0100

    remove dead validator code

--- crates/ev-cli/src/commands/interact.rs
+++ crates/ev-cli/src/commands/interact.rs
@@ -9,20 +9,12 @@ pub mod validators {
 
     #[derive(Debug)]
     pub enum ValidationError {
-        InvalidHostname,
-        InvalidCustomDomain,
         InvalidDestinationDomain,
     }
     impl Display for ValidationError {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             #[allow(unreachable_patterns)]
             match self {
-                Self::InvalidHostname => write!(f, "Value must be a valid HTTPS domain"),
-                Self::InvalidCustomDomain => write!(
-                    f,
-                    "Invalid custom domain. You should not use a \
-                 top-level domain name, or include a protocol or path"
-                ),
                 Self::InvalidDestinationDomain => write!(
                     f,
                     "Destination Domain must be a valid hostname and not contain a protocol or path."
@@ -33,30 +25,6 @@ pub mod validators {
     }
 
     pub type GenericValidator = dyn Fn(&String) -> Result<(), ValidationError>;
-    pub fn is_valid_hostname(input: &String) -> Result<(), ValidationError> {
-        lazy_static::lazy_static!(
-            static ref VALID_HOST_REGEX: Regex = Regex::new(
-                r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$|^(https://)?(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)+([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$"
-            ).unwrap();
-        );
-        match VALID_HOST_REGEX.is_match(input) {
-            true => Ok(()),
-            _ => Err(ValidationError::InvalidHostname),
-        }
-    }
-
-    pub fn is_valid_custom_domain(input: &String) -> Result<(), ValidationError> {
-        lazy_static::lazy_static!(
-            static ref VALID_CUSTOM_DOMAIN_REGEX: Regex = Regex::new(
-                // top-level domains not supported
-                r"(?i)^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$|^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9])\.){2,}([A-Za-z]|[A-Za-z][A-Za-z0-9-]*[A-Za-z0-9])$"
-            ).unwrap();
-        );
-        match VALID_CUSTOM_DOMAIN_REGEX.is_match(input) {
-            true => Ok(()),
-            _ => Err(ValidationError::InvalidCustomDomain),
-        }
-    }
 
     pub fn is_valid_destination_domain(input: &String) -> Result<(), ValidationError> {
         lazy_static::lazy_static!(
