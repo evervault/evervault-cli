9c7086e646d346d18c82e57ff7ea77667b3826df
Mark Doyle <39464373+doylemark@users.noreply.github.com>
2024-05-27 15:57:52 +0100

    fix update cmd

--- crates/ev-cli/src/commands/mod.rs
+++ crates/ev-cli/src/commands/mod.rs
@@ -1,7 +1,7 @@
+use self::{enclave::EnclaveArgs, function::FunctionArgs, relay::RelayArgs, update::UpdateArgs};
+use super::run_cmd;
 use crate::{print_and_exit, BaseArgs};
 use clap::Parser;
-
-use self::{enclave::EnclaveArgs, function::FunctionArgs, relay::RelayArgs, update::UpdateArgs};
 
 mod enclave;
 mod function;
@@ -19,12 +19,13 @@ pub enum Command {
 
 pub async fn run(base_args: BaseArgs) {
     if let Ok(Some(version_msg)) = crate::version::check_version().await {
-        print_and_exit(version_msg);
+        print_and_exit(version_msg, true);
     };
 
     match base_args.command {
         Command::Enclave(enclave_args) => enclave::run(enclave_args).await,
         Command::Relay(relay_args) => relay::run(relay_args).await,
         Command::Function(function_args) => function::run(function_args).await,
+        Command::Update(update_args) => run_cmd(update::run(update_args).await),
     }
 }
