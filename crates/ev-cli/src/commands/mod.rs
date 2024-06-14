62336f6afdbc8fec81a6cea47bdb322bd49380b1
Mark Doyle <mark@mdoyle.sh>
2024-06-14 19:29:38 +0100

    Fix Bad auth error (#139)
    * fix auth error
    
    * fix decrypt json parsing
    
    * don't check auth for update cmd
    
    * remove infallibe update matcher

--- crates/ev-cli/src/commands/mod.rs
+++ crates/ev-cli/src/commands/mod.rs
@@ -29,13 +29,19 @@ pub async fn run(base_args: BaseArgs) {
         print_and_exit(version_msg, true);
     };
 
+    match base_args.command {
+        Command::Update(update_args) => run_cmd(update::run(update_args).await),
+        _ => {}
+    }
+
     let auth = crate::get_auth();
+
     match base_args.command {
         Command::Enclave(enclave_args) => enclave::run(enclave_args, auth).await,
         Command::Relay(relay_args) => relay::run(relay_args, auth).await,
         Command::Function(function_args) => function::run(function_args, auth).await,
-        Command::Update(update_args) => run_cmd(update::run(update_args).await),
         Command::Encrypt(encrypt_args) => run_cmd(encrypt::run(encrypt_args, auth).await),
         Command::Decrypt(decrypt_args) => run_cmd(decrypt::run(decrypt_args, auth).await),
+        Command::Update(_) => unreachable!("infallible: matched previously"),
     }
 }
